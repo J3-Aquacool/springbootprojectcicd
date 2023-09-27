a804cb92855494e6acae9e20373ac0a86827cc9e
J3-Aquacool <jagdish.java2@gmail.com>
2023-09-27 16:28:46 +0530

    First Commit

---
+++ src/main/java/com/example/demo/SpringBootGitHubIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitHubIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitHubIntegrationApplication.class, args);
+	}
+
+}
